27d74a02c44c311ae46aab7d18b8c38d07f5c8ea
Maxim Shemyakin Olegovich <doretti.female@mail.ru>
2022-04-06 00:48:22 +0300

    fix project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,12 @@ class ProjectsController < ApplicationController
     def index 
         render json: Project.all
     end
+
+    def create
+        render json: Project.create(project_params)
+    end
+
+    private def project_params 
+        params.require(:project).permit(:title)
+    end
 end
